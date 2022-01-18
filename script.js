a98cd2958d351fc0cd5ebb3c4ce657b497cc4016
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-18 12:37:18 -0500

    Working with classes to open and close modal

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+const openModal = function () {
+  console.log('Button click');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  console.log(btnsOpenModal[i].addEventListener('click', openModal));
 }
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
